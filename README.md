42be4af0005afcf52b388279e9823f7e91b0ae94
RomanceChika <romance.yamada@gmail.com>
2021-10-09 13:28:23 +0900

    音声結合処理の実装

--- README.md
+++ README.md
@@ -5,6 +5,23 @@
 ## 環境構築
 
 1. SOXのインストール
-2. Pythonの環境構築
-3. requirements.txtのインストール
+2. ffmpegのインストール
+3. Pythonの環境構築
+4. requirements.txtのインストール
+5. サーバの設定(nginxなど)
 
+## 使い方
+
+- 録音
+
+recording/main.pyを実行すると1分ごとに録音が行われる  
+24時間以降は上書きされる
+
+- 取得
+
+api/run.pyでAPIを起動  
+
+#### エンドポイント
+
+/donwload
+/downloadエンドポイントにGETリクエストを送ると
---
+++ api/lib/sound_connector.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+
+from pydub import AudioSegment
+
+from api.path_manager import PathManager
+
+
+class SoundConnector(object):
+    """
+    音声ファイルを連結するためのクラス
+    """
+
+    @staticmethod
+    def connect_sound(file_path_list: list, file_name: str):
+        """
+        指定されたパスの音声を連結して保存する
+        Args:
+            file_path_list: 音声のパスのリスト
+            file_name: 連結した後のファイル名
+        """
+        full_sound = AudioSegment.empty()
+        for file_path in file_path_list:
+            sound = AudioSegment.from_mp3(str(file_path))
+            full_sound += sound
+
+        output_dir_path = PathManager.root_path() / 'tmp'
+        shutil.rmtree(str(output_dir_path))
+        os.makedirs(str(output_dir_path), exist_ok=True)
+        output_path = output_dir_path / f"{file_name}.mp3"
+        print(type(full_sound))
+        with output_path.open(mode='wb') as f:
+            full_sound.export(f, format='mp3')
+
+if __name__ == '__main__':
+    from file_selector import FileSelector
+    fs = FileSelector(4)
+    sf_list = fs.select_files()
+    print(sf_list)
+    SoundConnector.connect_sound(sf_list, 'testconcat')
